00932db324c750493772cbe597a7683c17f06dbd
unknown <abelputra101@gmail.com>
2026-01-29 21:08:33 +0700

    added apk extraction list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    //Swipe Refresh
+    implementation(libs.androidx.swiperefreshlayout)
 }
